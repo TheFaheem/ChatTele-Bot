87638342dd2cba68a74ff9371d7c43f3c5867191
Mohammed Faheem <104909089+TheFaheem@users.noreply.github.com>
2023-01-07 00:20:52 +0530

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -14,20 +14,20 @@ def chatbot(message):
     name = f"{message.from_user.first_name} {message.from_user.last_name}"
     msg = f"Hi {name}!\nEnter The Question You Want To Ask."
     bot.send_message(message.chat.id, msg)
-    @bot.message_handler(content_types='text')
-    def gpt(message):
-        text = openai.Completion.create(
-        model="text-davinci-003",
-        prompt= message.text,
-        temperature=0.7,
-        max_tokens= 256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.0)
-        response= (text)["choices"][0]['text'][2:]
-        bot.send_message(message.chat.id, response)
+        @bot.message_handler(content_types='text')
+        def gpt(message):
+            text = openai.Completion.create(
+            model="text-davinci-003",
+            prompt= message.text,
+            temperature=0.7,
+            max_tokens= 256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.0)
+            response= (text)["choices"][0]['text'][2:]
+            bot.send_message(message.chat.id, response)
         
-bot.polling(none_stop=True)
+bot.polling()
     
 
     
